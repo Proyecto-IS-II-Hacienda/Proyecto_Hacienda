daa39007c03035a11bdcd96de2f6dd40719af5af
Albert <Albert@Lenovo-PC>
2019-10-20 11:41:16 -0500

    Modulo funcional

--- ProyectoHacienda/ProyectoHacienda-web/src/main/java/grupo6/managed_beans/ProductosManagedBean.java
+++ ProyectoHacienda/ProyectoHacienda-web/src/main/java/grupo6/managed_beans/ProductosManagedBean.java
@@ -14,19 +14,20 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 
 /**
  *
  * @author Albert
  */
 @Named(value = "productosManagedBean")
-@RequestScoped
+@ViewScoped
 public class ProductosManagedBean implements Serializable, Crud<Productos> {
     private List<Productos> productosList;
     @EJB
     private ProductosFacadeLocal productosFacadeLocal;
     private Productos producto;
+    private boolean esNuevo;
    
     /**
      * Creates a new instance of ProductosManagedBean
@@ -37,12 +38,16 @@ public class ProductosManagedBean implements Serializable, Crud<Productos> {
     @PostConstruct
     @Override
     public void init(){
+       productosList=null;
        productosList=productosFacadeLocal.findAll();
+       producto=null;
+       esNuevo=false;
     }
     
     @Override
     public void nuevo(){
         producto=new Productos();
+        esNuevo=true;
     }
     
     public List<Productos> getProductosList() {
@@ -64,17 +69,25 @@ public class ProductosManagedBean implements Serializable, Crud<Productos> {
     @Override
     public void grabar() {
         try{
-            if (producto.getIdproducto() == null) {
+            if (esNuevo) {
+                producto.setIdproducto(producto.getIdproducto().toUpperCase());
+                for (int i = 0; i < producto.getIdproducto().length(); i++) {
+                    if (producto.getIdproducto().length() == 10) {
+                        break;
+                    } else {
+                        producto.setIdproducto(producto.getIdproducto()+"0");
+                    } 
+                }
                 productosFacadeLocal.create(producto);
             } else {
                 productosFacadeLocal.edit(producto);
             }
             init();
             producto=null;
-            notificarExito("La factura ha sido guardada exitosamente");
+            notificarExito("El producto ha sido guardado exitosamente");
 
         }catch(Exception e){
-            notificarError("Ocurrio un error al grabar la factura");
+            notificarError("Ocurrio un error al grabar el producto");
         }
         
     }
@@ -89,15 +102,20 @@ public class ProductosManagedBean implements Serializable, Crud<Productos> {
         try{
             productosFacadeLocal.remove(clase);
             init();
-            notificarExito("La factura ha sido eliminada exitosamente");
+            notificarExito("El producto ha sido eliminado exitosamente");
         }catch(Exception e){
-            notificarError("Ocurrio un error al grabar la factura");
+            notificarError("Ocurrio un error al eliminar el producto");
 
+        }finally {
+            producto = null;
+            esNuevo = false;
+            init();
         }
     }
 
     @Override
     public void seleccionar(Productos clase) {
+        esNuevo=false;
         producto=clase;
     }
 }
